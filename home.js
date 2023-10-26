1a0a266c4359570ac2d9b9859475f6bfe8346209
Santino Bertoia <131382765+BerserkLover19@users.noreply.github.com>
2023-10-26 15:35:40 -0300

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-const iconoButton = document.querySelector(".navbar-icono");
-const navbar = document.querySelector(".navbar");
-
-function toggleNavbarLinks() {
-    navbar.classList.toggle("active");
-}
-
-iconoButton.addEventListener("click", toggleNavbarLinks);
